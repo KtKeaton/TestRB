116aafe9d356f8263ce23e8d52ea36e815542bdb
KtKeaton <ktr01@protonmail.com>
2021-11-21 15:45:47 +0800

    類別方法能否等同實體方法

---
+++ 1classdef.rb
@@ -0,0 +1,8 @@
+class Taiwan
+  def set(city)
+    puts "Set New City #{city}"
+  end
+end
+
+mayor = Taiwan.new.set("Shitome")
+# mayor.set("Shitome")
--- each_with_index.rb
+++ each_with_index.rb
@@ -3,4 +3,4 @@ array = ["a", "b", "c"]
 array.each_with_index {|obj, idx|
   p [obj, idx]}
 
-# 元素obj 與 索引 idx 都要帶
+# 元素obj 與 索引idx 都要帶
