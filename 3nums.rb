44975a73f591f14cdf90886444b4ef33ddb346d2
KtKeaton <ktr01@protonmail.com>
2021-11-23 22:00:22 +0800

    3sum測試

---
+++ 3nums.rb
@@ -0,0 +1,25 @@
+def three_sum(nums)
+    result = []
+        nums.sort
+        for i in nums
+            if nums[i] > 0
+                return result
+            if i > 0 and nums[i] == nums[i-1]
+                continue
+            left = i + 1
+            right = len(nums) - 1
+            while left < right
+                if nums[i] + nums[left] + nums[right] == 0
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left + 1]
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]
+                        right -= 1
+                    left += 1
+                    right -= 1
+                elsif nums[i] + nums[left] + nums[right] > 0
+                    right -= 1
+                else
+                    left += 1
+        return result
+end
