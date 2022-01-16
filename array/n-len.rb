cc26417db639c959103fe617c13cfc9d7e98574f
KtKeaton <ktr01@protonmail.com>
2022-01-16 11:19:49 +0800

    陣列方法

---
+++ array/n-len.rb
@@ -0,0 +1,4 @@
+# 從a[n]開始，獲取len個元素
+alpha = ["a", "b", "c", "d", "e"]
+p alpha[2, 2]
+p alpha[1, 3]
---
+++ array/replace.rb
@@ -0,0 +1,5 @@
+alpha = ["a", "b", "c", "d", "e"]
+alpha[1] = "BETA"
+alpha[4] = "EXTRA"
+
+p alpha
