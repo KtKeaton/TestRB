ac93ccbcf6d68b606f5dd5ef954166c550c371f4
KtKeaton <ktr01@protonmail.com>
2022-01-30 22:37:04 +0800

    區塊proc累加處理

--- block/total2.rb
+++ block/total2.rb
@@ -1,6 +1,6 @@
 def total2(from, to, &block)    # 定義一個proc參數，即變數名前加&，一定要在最後
     result = 0 
-    from.upto(to) |num|         # 從小到大排列後，處理從from到to的值
+    from.upto(to) do |num|      # 從小到大排列後，處理從from到to的值
         if block                # 如果有block
             result +=           ## 累加block處理過的值
                 block.call(num) ## 相當於前例的yield
