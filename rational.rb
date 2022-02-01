8381409f42fd741f70ef82de6e7b76dcc31aef18
KtKeaton <ktr01@protonmail.com>
2022-02-01 19:32:30 +0800

    算術運算 分數

--- rational.rb
+++ rational.rb
@@ -6,4 +6,4 @@ p [a, b]
 c = a + b
 p c                             #分數相加，自動轉分母
 p c.to_f                        #轉為浮點數
-p [c.numerator, c.denominator]  #抓出分子與分母
+p [c.numerator, c.denominator]  #抓出分子與分母。
