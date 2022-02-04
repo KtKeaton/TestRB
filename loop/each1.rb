487e9af9e433c3a4a5dd25fcca5aeaed79b39895
KtKeaton <ktr01@protonmail.com>
2022-02-04 22:18:38 +0800

    each方法

--- loop/each1.rb
+++ loop/each1.rb
@@ -1,5 +1,6 @@
-result = [1, 2, 3].each do |number|
-  puts "hi"
+sum = 0
+(1..5).each do |i|    #i依序傳入1,2,3,4,5
+    # sum= sum + i
+    sum += i          #sum等於1+2+3+4+5
 end
-
-puts result
+puts sum
