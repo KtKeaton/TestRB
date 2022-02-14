b9101160be91ef65b8e3a7700b4abd123b1495c8
KtKeaton <ktr01@protonmail.com>
2022-02-14 22:53:00 +0800

    計數step補正

--- count/step.rb
+++ count/step.rb
@@ -1,13 +1,15 @@
+##從from開始對i加step(第二個參數)處理，直到i等於to
+
 # ary = []
-# 2.step(10, 3) do |i|
-#   ary << i
+# 2.step(10, 3) do |i|    #若step為正，from比to大就停止
+#   ary << i              #從2開始每次加3，直到比10大
 # end
 
 # p ary
 
 ary = []
-2.step(2, -3) do |i|
-  ary << i
+10.step(2, -3) do |i|    #若step為負，from比to小就停止
+  ary << i              #從10開始每次減3，直到比2大
 end
 
 p ary
