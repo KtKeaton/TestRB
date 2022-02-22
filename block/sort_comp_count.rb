a9a8a9fbe4bacc4a4f2f274c94839e2b1706c221
KtKeaton <ktr01@protonmail.com>
2022-02-22 22:41:20 +0800

    補充說明

--- block/sort_comp_count.rb
+++ block/sort_comp_count.rb
@@ -1,10 +1,11 @@
-#按元素長度排列
+#按元素長度排列5
+# https://ruby-doc.org/core-3.0.2/Enumerable.html#method-i-sort
 ary = %w(
     Ruby is a open source programming language with a afocus on simplicity and productivity.
 )
 
-call_num = 0 #呼叫block次數
-sorted = ary.sort do |a, b|
+call_num = 0                    #呼叫block次數
+sorted = ary.sort do |a, b|     #先讀進來的2個字丟進參數a與b ##等於ary.sort_by { |a| a.length}
     call_num += 1
     a.length <=> b.length
 end
