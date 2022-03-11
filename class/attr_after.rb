f9fdbe0e1e95d1ffa708388db0a10d237be8e601
KtKeaton <ktr01@protonmail.com>
2022-03-11 23:06:16 +0800

    attr_accessor補充

--- class/attr_after.rb
+++ class/attr_after.rb
@@ -1,13 +1,16 @@
-#存取器
-##物件外部不能抓實體變數&變數值
+# 存取器
+## 物件外部不能抓實體變數&變數值
 class HelloWorld
-  attr_accessor :name #獲取及修改@name，定義了name與name=方法
+  attr_accessor :name 
+  # 原本的getter與settter，分別定義了「實體方法name與name=」
+  # 也就是 def name 不是 def self.name，所以不是類別方法
+  # accessor讓「類別HelloWorld」及其新生實體，可以用「實體方法name與name=」
 
-  def initialize(name = "實體變數預設值")     #初始化，需要生成實體變數，給新實體用
+  def initialize(name = "實體方法=變數預設值")     # 初始化，需要生成實體變數，給新實體用
     @name = name
   end
 
-  def hello           #可以使用的方法
+  def hello           # 可以使用的方法
     puts "Hello, I am #{@name}"
   end
 
@@ -16,7 +19,7 @@ class HelloWorld
   end
 
   # 存取器取代以下兩個方法
-  # 這是getter，
+  # 這是getter
   # def name            
   #     @name
   # end
@@ -28,7 +31,13 @@ class HelloWorld
 
 end
 
-keaton = HelloWorld.new()           #生成新實體，傳入參數「初始化」之後才有實體變數用
+# 類別生成新實體，傳入參數「初始化」之後才有實體變數用
+keaton = HelloWorld.new("舊名字")
+
+# 若沒有getter，無法用「實體方法 name」
+keaton.name
+
+# 若沒有setter，無法用「實體方法 name =」
 keaton.name = "重新給個名字"
-keaton.hello                        #使用hello實體方法，而不是讀取「name屬性」，Ruby沒有屬性
-keaton.intro                        #使用intro實體方法，同上
+keaton.hello                        # 使用hello實體方法，而不是讀取「name屬性」，Ruby沒有屬性
+keaton.intro                        # 使用intro實體方法，同上
