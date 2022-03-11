7fcb5cb0d565d505fec0c4a4bdb825246a44bf26
KtKeaton <ktr01@protonmail.com>
2022-03-11 16:49:00 +0800

    Array排列補充

--- array/arr_sort.rb
+++ array/arr_sort.rb
@@ -1,5 +1,5 @@
 array = ["Ruby", "Perl", "PHP", "Python"]
-sorted = array.sort #按大小寫排列、建議
-# sorted = array.sort{ |a, b| a<=>b } #按大小寫排列
+# sorted = array.sort #按大小寫排列、建議
+sorted = array.sort{ |a, b| a<=>b } #一次抓兩個值比較、按大小寫排列
 
 p sorted
--- array/arr_sort2.rb
+++ array/arr_sort2.rb
@@ -1,5 +1,5 @@
 array = ["Ruby", "Perl", "PHP", "Python"]
-# sorted = array.sort{ |a, b| b.length<=>a.length } #按長度排列
+# sorted = array.sort{ |a, b| b.length<=>a.length } #一次抓兩個值比較、按長度排列
 sorted = array.sort{ |item| item.length }  #按長度排列、建議
 
 p sorted
