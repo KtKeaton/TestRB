a97092ad2859a17e5c8c2d01932f372da2f06735
KtKeaton <ktr01@protonmail.com>
2022-03-13 14:36:49 +0800

    enum chunk相鄰奇數偶數

--- enum/chunk_while.rb
+++ enum/chunk_while.rb
@@ -1,4 +1,4 @@
-# 將偶數包在一起，輸出array
+# 判定相鄰的奇數偶數，輸出array
 
 a = [7, 5, 9, 2, 0, 7, 9, 4, 2, 0]
-p a.chunk_while {|i, j| i.even? == j.even? }.to_a
+p a.chunk_while {|i, j| i.even? == j.even? }.to_a   # 若相鄰參數為偶數，包起來
