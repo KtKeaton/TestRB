2f475fa4cd482941f07f5d8e4dffcfb27bd460dd
KtKeaton <ktr01@protonmail.com>
2022-04-13 19:31:41 +0800

    區塊proc呼叫參數平方 修正

--- block/proc2.rb
+++ block/proc2.rb
@@ -1,12 +1,12 @@
 square = Proc.new do |n|    # 生成block並物件化
-  n ** 2                    # 變數＝>變數的平方
+  n ** 2                    # 參數＝>參數的平方
 end
 
 class Array
-  def iterate!(code)
-    self.map {|n| code.call(n)}   # 呼叫block
+  def iterate!(code)        # code 就是 square
+    self.map {|n| code.call(n)}   # 呼叫block   # n 就是 self 的陣列元素
   end
 end
 
-puts [1, 2, 3].iterate!(square)
-puts [4, 5, 6].iterate!(square)
+puts [1, 2, 3].iterate!(square) # square 帶入一個整數參數（的平方），源自block
+puts [4, 5, 6].iterate!(square) # square 帶入一個整數參數（的平方），源自block
