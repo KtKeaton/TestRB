f647ba43cd9509fa875b201143a1834178c18d30
KtKeaton <ktr01@protonmail.com>
2022-04-23 16:54:49 +0800

    新增private方法

--- spec_atm.rb
+++ spec_atm.rb
@@ -1,5 +1,5 @@
 class ATM
-  attr_reader :attr_names
+  attr_reader :balance
 
   def initialize(balance)
     @balance = balance
@@ -14,7 +14,7 @@ class ATM
   # end
 
   def withdraw(money)
-    if money > 0 && money <= @balance
+    if money > 0 && enough?(money)
       @balance = @balance - money
     return money
     else
@@ -23,28 +23,35 @@ class ATM
 
 end
 
-RSpec.describe ATM do 
-  describe "存錢功能" do
-    it "領錢" do
-      atm = ATM.new(10) #Arrange
-      atm.deposit(20) #Act
-      expect(atm.balance).to be 30 #Assert
+  RSpec.describe ATM do 
+    describe "存錢功能" do
+      it "領錢" do
+        atm = ATM.new(10) #Arrange
+        atm.deposit(20) #Act
+        expect(atm.balance).to be 30 #Assert
+      end
+
+      it "不可存0元或小於0元" do
+        atm = ATM.new(10) #Arrange
+        atm.deposit(-10) #Act
+        expect(atm.balance).to be 10 #Assert
+      end
     end
 
-    it "不可存0元或小於0元" do
-      atm = ATM.new(10) #Arrange
-      atm.deposit(-10) #Act
-      expect(atm.balance).to be 10 #Assert
+    describe "領錢功能" do
+      it "可以領錢" do
+        atm = ATM.new(20) #Arrange
+        money = atm.withdraw(10) #Act
+        
+        expect(atm.balance).to be 10 #Assert
+        expect(money).to be 10 #Assert
+      end
     end
   end
 
-  describe "領錢功能" do
-    it "可以領錢" do
-      atm = ATM.new(20) #Arrange
-      money = atm.withdraw(10) #Act
-      
-      expect(atm.balance).to be 10 #Assert
-      expect(money).to be 10 #Assert
-    end
-  end
+  priavte
+  def enough?(money)
+    money <= @balance
+  end 
+
 end
